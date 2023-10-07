91d3360b99a53dba1119b5e5af6506b43e6ea6ec
kauanr0d <kauankenney16752@gmail.com>
2023-10-07 20:25:47 -0300

    ambiente virtual ativado

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def upload():
     file.save("static/imagem.jpg")
 
     imagem = abrir_imagem("static/imagem.jpg")
-    objetos, imagem_detectada = detectar(model=modelo, imagem=imagem, classe_objeto='book', class_names=class_names)
+    objetos, imagem_detectada = detectar(model=modelo, imagem=imagem, classe_objeto= "book", class_names=class_names)
    
     cv2.imwrite("static/imagem_detectada.jpg", imagem_detectada)
     saida_tts(objetos)
